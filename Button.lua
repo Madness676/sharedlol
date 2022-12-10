88112a79e6ffff263668e3b91e17bb8909afc51d
Madness676 <117950457+Madness676@users.noreply.github.com>
2022-12-10 21:33:44 +0800

    Update Button.lua

--- Button.lua
+++ Button.lua
@@ -12,14 +12,14 @@ Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(105, 105, 105)
 Frame.BackgroundTransparency = 0.10
 Frame.Position = UDim2.new(0.858712733, 0, 0.0237762257, 0)
-Frame.Size = UDim2.new(0.1, 0.15, 0.15, 0.15)
+Frame.Size = UDim2.new(0.70, 0.15, 0.70, 0.70)
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(105, 105, 105)
 TextButton.BackgroundTransparency = 0.10
 TextButton.Size = UDim2.new(1, 0, 1, 0)
 TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "Open/Close"
+TextButton.Text = "Open"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.TextScaled = true
 TextButton.TextSize = 50.000
